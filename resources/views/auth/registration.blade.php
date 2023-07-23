d25db0d958462568545e4187cf682149935840aa
frhnakbr <frhnakbr@gmail.com>
2023-07-23 16:42:05 +0700

    Mengubah Halaman Login

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -22,7 +22,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                <label for="email_address" class="col-md-4 col-form-label text-md-right">Email</label>
                                 <div class="col-md-6">
                                     <input type="text" id="email_address" class="form-control" name="email" required autofocus>
                                     @if ($errors->has('email'))
@@ -45,7 +45,7 @@
                                 <div class="col-md-6 offset-md-4">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember"> Remember Me
+                                            <input type="checkbox" name="remember"> Ingat Saya
                                         </label>
                                     </div>
                                 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -203,9 +203,9 @@
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
                     <h1 class="fw-light">Jadwal Mengajar Dosen</h1>
-                    <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                    <p class="lead text-body-secondary">Jadwal Dosen Universitas Muhammadiyah Pontianak.</p>
                     <p>
-                        <a href="/login" class="btn btn-primary my-2">Login Page</a>
+                        <a href="/login" class="btn btn-primary my-2">Halaman Login</a>
                         
                     </p>
                 </div>
