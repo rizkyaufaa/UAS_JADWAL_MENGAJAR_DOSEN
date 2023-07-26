015807064ea8a4d9c201e177283b9ae8f80c8d86
rizkyaufaa <rizkyaufavitasarii@gmail.com>
2023-07-26 14:35:03 +0700

    mengubah components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,7 +56,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
         <div class="container">
-            <a class="navbar-brand" href="#">Jadwal Mengajar Dosen</a>
+            <a class="navbar-brand text-white" href="#">Universitas Muhammadiyah Pontianak</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
