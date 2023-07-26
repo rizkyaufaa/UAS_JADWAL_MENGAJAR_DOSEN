df3fa4d05bdb6f61e72ef2a81b9527e9e7667b44
frhnakbr <frhnakbr@gmail.com>
2023-07-26 22:06:27 +0700

    menghubungkan sidebar

--- resources/views/fullcalender.blade.php
+++ resources/views/fullcalender.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>jadwal mengajar dosen</title>
+    <title>Jadwal</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
